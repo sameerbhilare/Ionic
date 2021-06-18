33ceb828945a4a535a0e44fc658edf995df95e53
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-18 18:29:50 +0530

    Validating User Input

--- Workspace/budget-planner/app.js
+++ Workspace/budget-planner/app.js
@@ -4,5 +4,13 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 
 confirmBtn.addEventListener('click', () => {
-  console.log('It works!');
+  const enteredReason = reasonInput.value;
+  const enteredAmount = amountInput.value;
+
+  // validation
+  if (enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim().length <= 0) {
+    return;
+  }
+
+  console.log(enteredReason, enteredAmount);
 });
