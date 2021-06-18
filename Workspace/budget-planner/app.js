4ae8ff42a2694d8846e1cd15b0e891ce000a4a64
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-18 18:39:08 +0530

    Creating Ionic Elements Programmatically

--- Workspace/budget-planner/app.js
+++ Workspace/budget-planner/app.js
@@ -2,6 +2,7 @@ const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
 
 confirmBtn.addEventListener('click', () => {
   const enteredReason = reasonInput.value;
@@ -12,5 +13,10 @@ confirmBtn.addEventListener('click', () => {
     return;
   }
 
-  console.log(enteredReason, enteredAmount);
+  // console.log(enteredReason, enteredAmount);
+  // create ionic element (just like regular html elements)
+  const newItem = document.createElement('ion-item');
+  newItem.textContent = enteredReason + ': $' + enteredAmount;
+  // append it to expense list
+  expensesList.appendChild(newItem);
 });
