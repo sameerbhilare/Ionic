4dbd7e2bf1130cbac2c21c4bd7a62873ca63cca7
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-21 12:05:46 +0530

    Using Angular Features on Ionic Components

--- Workspace/ionic-angular-course/src/app/recipes/recipes.page.ts
+++ Workspace/ionic-angular-course/src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Chicken Burger',
+      imageUrl:
+        'https://media.istockphoto.com/photos/chicken-burger-picture-id172449461',
+      ingredients: ['French Fries', 'Chicken', 'Cheese'],
+    },
+    {
+      id: 'r2',
+      title: 'French Fries',
+      imageUrl:
+        'https://static.toiimg.com/thumb/54659021.cms?imgsize=275086&width=509&height=340',
+      ingredients: ['French Fries', 'Potato'],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
