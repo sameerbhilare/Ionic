42e4c23d7e02460fbc36d2e714d01e6d338cfaca
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-22 19:34:43 +0530

    Going Back with NavController

--- Workspace/book-place-app/src/app/places/discover/place-detail/place-detail.page.ts
+++ Workspace/book-place-app/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  constructor(private router: Router, private navCtrl: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onBookPlace() {
+    //this.router.navigateByUrl('/places/tabs/discover');
+
+    /*
+      NavController - under the hood will use angular router
+      but will play proper animation based on forward or backward navigation
+    */
+    // way 1
+    this.navCtrl.navigateBack('/places/tabs/discover');
+
+    // way 2 - unreliable
+    // does not work if stack is empty. So wont work is app starts from this page
+    // ADVANTAGE - is we dont have to specify path/URL
+    // this.navCtrl.pop();
   }
-
 }
