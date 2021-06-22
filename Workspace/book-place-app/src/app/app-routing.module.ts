ed29f415c0c2b31d2edfd5997006b4a0839ab4b5
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-22 16:26:12 +0530

    Adjusting Our Main Routing Configuration

--- Workspace/book-place-app/src/app/app-routing.module.ts
+++ Workspace/book-place-app/src/app/app-routing.module.ts
@@ -4,16 +4,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'recipes',
+    redirectTo: 'places',
     pathMatch: 'full',
   },
   {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((m) => m.AuthPageModule),
+  },
+  {
     path: 'places',
-    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+    loadChildren: () =>
+      import('./places/places.module').then((m) => m.PlacesPageModule),
   },
   {
     path: 'bookings',
-    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
+    loadChildren: () =>
+      import('./bookings/bookings.module').then((m) => m.BookingsPageModule),
   },
 ];
 
