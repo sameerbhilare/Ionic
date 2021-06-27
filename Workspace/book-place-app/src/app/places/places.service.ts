5ac2cadb16d3f47463705aeb44da483a9a21483f
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-27 14:09:10 +0530

    Adding userId for place model

--- Workspace/book-place-app/src/app/places/places.service.ts
+++ Workspace/book-place-app/src/app/places/places.service.ts
@@ -15,7 +15,8 @@ export class PlacesService {
       'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
       149.99,
       new Date('2021-01-01'),
-      new Date('2021-12-31')
+      new Date('2021-12-31'),
+      'abc'
     ),
     new Place(
       'p2',
@@ -24,7 +25,8 @@ export class PlacesService {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
       189.99,
       new Date('2021-01-01'),
-      new Date('2021-12-31')
+      new Date('2021-12-31'),
+      'abc'
     ),
     new Place(
       'p3',
@@ -33,7 +35,8 @@ export class PlacesService {
       'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
       99.99,
       new Date('2021-01-01'),
-      new Date('2021-12-31')
+      new Date('2021-12-31'),
+      'abc'
     ),
   ];
 
