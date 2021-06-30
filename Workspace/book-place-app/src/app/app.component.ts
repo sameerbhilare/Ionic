451328c086449614c274be903cd11cb73b107ae5
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-30 10:30:23 +0530

    Using Capacitor Plugins - SplashScreen

--- Workspace/book-place-app/src/app/app.component.ts
+++ Workspace/book-place-app/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { Router } from '@angular/router';
+import { Capacitor } from '@capacitor/core';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +11,25 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private platform: Platform,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      if (Capacitor.isPluginAvailable('SpashScreen')) {
+        // hide the splashscreeen which automatically loads when the app boots
+        // A splash screen is mostly the first screen of the app when it is opened.
+        // It is a constant screen which appears for a specific amount of time,
+        // generally shows for the first time when the app is launched.
+        SplashScreen.hide();
+      }
+    });
+  }
 
   onLogout() {
     console.log('logout');
