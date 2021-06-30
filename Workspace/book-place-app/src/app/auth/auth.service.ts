277005d58419b84ac79256feba2a4eaa820318b2
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-30 18:57:46 +0530

    Adding Autologin method

--- Workspace/book-place-app/src/app/auth/auth.service.ts
+++ Workspace/book-place-app/src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Capacitor } from '@capacitor/core';
 import { Storage } from '@capacitor/storage';
 import { environment } from '../../environments/environment';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, from } from 'rxjs';
 import { User } from './user.model';
 import { map, tap } from 'rxjs/operators';
 
@@ -75,6 +76,53 @@ export class AuthService {
     this._user.next(null);
   }
 
+  // finds Auth data in storage and if it finds it and is valid, then automaticaly logs user in.
+  autoLogin() {
+    // from operator converts a Promise into an Observable
+    return from(Storage.get({ key: 'AuthData' })).pipe(
+      map((storedData) => {
+        if (!storedData || !storedData.value) {
+          return null;
+        }
+
+        // convert string to object and typecast
+        const parsedAuthData = JSON.parse(storedData.value) as {
+          userId: string;
+          email: string;
+          token: string;
+          tokenExpirationDate: string;
+        };
+
+        const tokenExpirationDate = new Date(
+          parsedAuthData.tokenExpirationDate
+        );
+        if (tokenExpirationDate <= new Date()) {
+          // token expired
+          return null;
+        }
+
+        // found required auth data, so create User and return
+        const user = new User(
+          parsedAuthData.userId,
+          parsedAuthData.email,
+          parsedAuthData.token,
+          tokenExpirationDate
+        );
+
+        return user;
+      }),
+      tap((user) => {
+        if (user) {
+          this._user.next(user);
+        }
+      }),
+      // to return true or false
+      map((user) => {
+        return !!user; // true if we have user else false
+      })
+    );
+  }
+
   private setUserData(authData: AuthResponseData) {
     // calculate expiration time
     // what we get in 'expiresIn' is 'The number of seconds in which the ID token expires.'
@@ -93,6 +141,7 @@ export class AuthService {
     // stora data in localstorate/device storage
     this.storeAuthData(
       authData.localId,
+      authData.email,
       authData.idToken,
       expirationTime.toISOString()
     );
@@ -101,10 +150,16 @@ export class AuthService {
   // storing imp data via Capacitor/Storage
   private storeAuthData(
     userId: string,
+    email: string,
     token: string,
     tokenExpirationDate: string
   ) {
-    const userData = JSON.stringify({ userId, token, tokenExpirationDate });
+    const userData = JSON.stringify({
+      userId,
+      email,
+      token,
+      tokenExpirationDate,
+    });
     // storage needs to string data only
     Storage.set({ key: 'AuthData', value: userData });
   }
